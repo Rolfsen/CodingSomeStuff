41739f82122f6af45b6a6d3b1ab929b0a5f8b02f
Unknown <rasmus.rolfsen@mail.dk>
2020-03-07 05:40:50 +0100

    Refacotring

---
+++ BowlingGame/BowlingTests/GivenLastRoundWithOnePlayer.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+
+namespace BowlingTests
+{
+    public class GivenLastRoundWithOnePlayer
+    {
+        [Fact]
+        public void Rolling3And4ShouldBeValid()
+        {
+            Turn turn = new Turn();
+            turn.Rolls.Add(3);
+            Assert.True(LastRoundValidator.ValidateLastRoundRolls(4, turn));
+        }
+        
+        [Fact]
+        public void Rolling10And4And7ShouldBeInvalid()
+        {
+            Turn turn = new Turn();
+            turn.Rolls.Add(10);
+            turn.Rolls.Add(6);
+            Assert.False(LastRoundValidator.ValidateLastRoundRolls(5, turn));
+        }
+        
+        [Fact]
+        public void Rolling3And8ShouldBeInvalid()
+        {
+            Turn turn = new Turn();
+            turn.Rolls.Add(3);
+            Assert.False(LastRoundValidator.ValidateLastRoundRolls(8, turn));
+        }
+        
+        [Fact]
+        public void Rolling1010And10ShouldBeValid()
+        {
+            Turn turn = new Turn();
+            turn.Rolls.Add(10);
+            turn.Rolls.Add(10);
+            Assert.True(LastRoundValidator.ValidateLastRoundRolls(10, turn));
+        }
+        
+    }
+}
--- BowlingGame/BowlingTests/LastRoundValidator.cs
+++ BowlingGame/BowlingTests/LastRoundValidator.cs
@@ -1,5 +1,4 @@
 ﻿using System.Linq;
-using Xunit;
 
 namespace BowlingTests
 {
@@ -27,43 +26,5 @@ namespace BowlingTests
 
             return rolls.First() + roll <= 10;
         }
-    }
-
-    public class GivenLastRoundWithOnePlayer
-    {
-        [Fact]
-        public void Rolling3And4ShouldBeValid()
-        {
-            Turn turn = new Turn();
-            turn.Rolls.Add(3);
-            Assert.True(LastRoundValidator.ValidateLastRoundRolls(4, turn));
-        }
-        
-        [Fact]
-        public void Rolling10And4And7ShouldBeInvalid()
-        {
-            Turn turn = new Turn();
-            turn.Rolls.Add(10);
-            turn.Rolls.Add(6);
-            Assert.False(LastRoundValidator.ValidateLastRoundRolls(5, turn));
-        }
-        
-        [Fact]
-        public void Rolling3And8ShouldBeInvalid()
-        {
-            Turn turn = new Turn();
-            turn.Rolls.Add(3);
-            Assert.False(LastRoundValidator.ValidateLastRoundRolls(8, turn));
-        }
-        
-        [Fact]
-        public void Rolling1010And10ShouldBeValid()
-        {
-            Turn turn = new Turn();
-            turn.Rolls.Add(10);
-            turn.Rolls.Add(10);
-            Assert.True(LastRoundValidator.ValidateLastRoundRolls(10, turn));
-        }
-        
     }
 }
