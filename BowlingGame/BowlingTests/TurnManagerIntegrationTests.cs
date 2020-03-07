469dad94e0acc2ac148ad116572f51c3987a9c16
Unknown <rasmus.rolfsen@mail.dk>
2020-03-07 05:57:44 +0100

    Add integration test class for game

---
+++ BowlingGame/BowlingTests/TurnManagerIntegrationTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace BowlingTests
+{
+    public class TurnManagerIntegrationTests
+    {
+        private PlayerTurnManager manager;
+        
+        public TurnManagerIntegrationTests()
+        {
+            this.manager = new PlayerTurnManager(new List<Player>()
+            {
+                new Player("P1"),
+                new Player("P2")
+            });
+        }
+        
+        
+        [Fact]
+        public void Game1 ()
+        {
+            List<int> rolls = new List<int>()
+            {
+                3,5,
+                10,
+                10,
+                10,
+                10,
+                10,
+                10,
+                10,
+                10,
+                10,
+                10,
+                10,
+                10,
+                10,
+                10,
+                10,
+                10,
+                10,
+                10,
+                10,
+                10,
+                10,
+                10,
+                10
+            };
+
+            foreach (var ro in rolls)
+            {
+                manager.Roll(ro);
+            }
+
+            var scoreCalculator = new BowlingScoreCalculator();
+            
+            Assert.Equal(300, scoreCalculator.GetScore(manager.Players[1].turns));
+            Assert.Equal(278, scoreCalculator.GetScore(manager.Players[0].turns));
+
+        }
+    }
+}
